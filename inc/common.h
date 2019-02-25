ae7a233ff6181501ad5e5566c12127186c11861a
Roberto Baquerizo <roba8460@colorado.edu>
2019-02-24 17:01:38 -0700

    defined default message sequence a and b

--- inc/common.h
+++ inc/common.h
@@ -6,7 +6,6 @@
 
 #define FIFO_SIZE    BODY_SIZE + HEADER_SIZE
 
-
 /* Defines type of message */
 typedef enum message {
    MESSAGE_BEGIN = 0,
@@ -21,6 +20,33 @@ typedef struct packet {
    char header[HEADER_SIZE];  /* Message header - contains PID of sender */
    char body[BODY_SIZE];      /* Actual contents for message */
 } packet_t;
+
+char *sequence_a[] = {
+   "First message in the sequence\n",
+   NULL,   /* Command */
+   NULL,   /* Command */
+   "Second string to send, but fourth message so far!\n",
+   "Two strings back to back\n",
+   NULL,   /* Command */
+   "Let's do three in a row!\n",
+   "Almost done...\n",
+   "This is the ninth message so far!\n",
+   NULL    /* Command */
+};
+
+char *sequence_b[] = {
+   "First message in the sequence\n",
+   "This is the second message so far!\n",
+   NULL,   /* Command */
+   NULL,   /* Command */
+   "Third string to send, but the fifth message so far!\n",
+   NULL,   /* Command */
+   "Finish up with only strings, I guess...\n",
+   "Filler, filler, filler, filler!\n",
+   "Second to last!\n",
+   "Wooo! Last message!\n"
+};
+
 
 
 
