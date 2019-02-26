627c2bf42d142af814d11f94e807f68a19916ca8
Roberto Baquerizo <roba8460@colorado.edu>
2019-02-25 22:11:58 -0700

    server + client socket - handle SGINT and SIGPIPE to exit gracefully

--- src/socket_one.c
+++ src/socket_one.c
@@ -5,8 +5,8 @@
 #include <unistd.h>
 #include <time.h>
 /* /sys includes */
-#include <sys/types.h>
-#include <sys/stat.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -17,20 +17,42 @@ static FILE     *log;
 static packet_t *msg_in;
 static packet_t *msg_out;
 
+static int server_fd, client_fd;
 static struct timespec thread_time;
 
-static void socket_exit(void)
+typedef enum reasons {
+   REASON_BEGIN,
+   REASON_SIGINT,
+   REASON_SIGPIPE,
+   REASON_MAX
+} reason_e;
+
+static void server_close( reason_e reason )
 {
    clock_gettime(CLOCK_REALTIME, &thread_time);
 
-   fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
-            getpid(), thread_time.tv_sec, thread_time.tv_nsec );
-   fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
-            getpid(), thread_time.tv_sec, thread_time.tv_nsec );
-
+   switch( reason )
+   {
+      case( REASON_SIGINT ):
+         fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec );
+         fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec ); 
+         break;
+      case( REASON_SIGPIPE ):
+         fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nConnection closed!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec );
+         fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nConnection closed!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec ); 
+         break;
+      default:
+         break;
+   }
    fclose( log );
    free( msg_in );
    free( msg_out );
+   close( server_fd );
+   close( client_fd );
    exit(0);
 }
 
@@ -38,74 +60,69 @@ void sig_handler(int signo)
 {
    if( signo == SIGINT )
    {
-      socket_exit();
+      server_close( REASON_SIGINT );
    }
-   else
+   else if( signo == SIGPIPE )
    {
-      fprintf( stdout, "PID %d caught unknown signal!\n",
-               getpid() );
-      fprintf( log, "PID %d caught unknown signal!\n",
-               getpid() );
+      server_close( REASON_SIGPIPE );
    }
    return;
 }
 
 int main(void)
 {
-   int sock, new_socket;
-   struct sockaddr_in my_addr;
-   int addrlen = sizeof(my_addr);
-
-   char *msg = sequence_a[0];
+   struct sockaddr_in serv_addr;
+   int addrlen = sizeof(serv_addr);
 
    int opt = 1;
 
    /* Set up signal handling */
    signal( SIGINT, sig_handler );
+   signal( SIGPIPE, sig_handler );
 
    /* Log file */
-   log = fopen( "first_pid.log", "w" );
+   log = fopen( "server.log", "w" );
 
    fprintf( log, "PID %d - Sockets:\n",
             getpid());
 
-   if( 0 > (sock = socket( AF_INET, SOCK_STREAM, 0 )) )
+   if( 0 > (server_fd = socket( AF_INET, SOCK_STREAM, 0 )) )
    {
       perror( "Encountered error creating socket" );
       return 1;
    }
 
-   if( setsockopt( sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt) ))
+   if( setsockopt( server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt) ))
    {
       perror( "Encountered error setting sock option" );
       return 1;
    }
-   my_addr.sin_family = AF_INET;
-   my_addr.sin_addr.s_addr = INADDR_ANY;
-   my_addr.sin_port = htons ( 8080 );
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons ( PORT );
 
    // Bind socket
-   if( 0 > bind( sock, (struct sockaddr *)&my_addr, sizeof(my_addr) ) )
+   if( 0 > bind( server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr) ) )
    {
       perror( "Encountered error binding socket" );
       return 1;
    }
 
    // Listen
-   if( 0 > listen( sock, 3 ) )
+   if( 0 > listen( server_fd, 3 ) )
    {
       perror( "Encountered error listening on socket" );
       return 1;
    }
 
-   if( 0 > (new_socket = accept(sock, (struct sockaddr *)&my_addr, (socklen_t*)&addrlen)) )
+   if( 0 > (client_fd = accept(server_fd, (struct sockaddr *)&serv_addr, (socklen_t*)&addrlen)) )
    {
       perror( "Encountered error accepting new connection" );
       return 1;
    }
 
    fprintf( log, "Socket ID: %d\nSocket Option: %d\nListening on port: %d\nFamily: %d\nSource Address: %d\n",
-            sock, opt, 8080, AF_INET, INADDR_ANY );
+            server_fd, opt, PORT, AF_INET, INADDR_ANY );
 
    /* Allocate memory for packet struct */
    msg_in = malloc( sizeof(packet_t) );
@@ -125,14 +142,6 @@ int main(void)
    int i = 0;
    while( 10 != i )
    {
-      read( new_socket, msg_in, sizeof(*msg_in) );
-
-      send( new_socket, msg, sizeof(*msg), 0 );
-      i++;
-
-      fprintf( log, "\n( %ld. %ld secs ) - Received:\nHeader: %sBody: %s",
-               thread_time.tv_sec, thread_time.tv_nsec, msg_in->header, msg_in->body );
-#if 0
       if( NULL != sequence_a[i] )
       {
          /* Write string to body of message */
@@ -148,10 +157,10 @@ int main(void)
          msg_out->type = MESSAGE_CMD;
       }
 
-      /* Write full message to pipe */
-      if( 0 > write( fout, msg_out, sizeof(*msg_out) ) )
+      /* Write full message to socket */
+      if( 0 > send( client_fd, msg_out, sizeof(*msg_out), 0) )
       {
-         perror( "Encountered error while attempting to write to FIFO!\n" );
+         perror( "Encountered error while attempting to write to socket" );
       }
 
       clock_gettime( CLOCK_REALTIME, &thread_time );
@@ -168,10 +177,10 @@ int main(void)
                   thread_time.tv_sec, thread_time.tv_nsec );
       }
 
-      /* Read from Input FIFO */
-      if( 0 > read( fin, msg_in, sizeof(*msg_in) ) )
+      /* Read from socket */
+      if( 0 > read( client_fd, msg_in, sizeof(*msg_in) ) )
       {
-         perror("Encountered error while atttempting to read FIFO!\n" );
+         perror("Encountered error while atttempting to read socket" );
       }
 
       clock_gettime( CLOCK_REALTIME, &thread_time );
@@ -192,7 +201,6 @@ int main(void)
       {
          i = 0;
       }
-#endif
    }
    return 0;
 }
--- src/socket_two.c
+++ src/socket_two.c
@@ -5,11 +5,11 @@
 #include <unistd.h>
 #include <time.h>
 /* /sys includes */
-#include <sys/types.h>
-#include <sys/stat.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-
+//#include <netinet/in.h>
+#include <arpa/inet.h>
 /* Local Inlcudes */
 #include "common.h"
 
@@ -17,20 +17,42 @@ static FILE     *log;
 static packet_t *msg_in;
 static packet_t *msg_out;
 
+static int sock_fd;
 static struct timespec thread_time;
 
-static void socket_exit(void)
+typedef enum reasons {
+   REASON_BEGIN,
+   REASON_SIGINT,
+   REASON_SIGPIPE,
+   REASON_MAX
+} reason_e;
+
+static void socket_exit( reason_e reason )
 {
    clock_gettime(CLOCK_REALTIME, &thread_time);
 
-   fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
-            getpid(), thread_time.tv_sec, thread_time.tv_nsec );
-   fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
-            getpid(), thread_time.tv_sec, thread_time.tv_nsec );
+   switch( reason )
+   {
+      case( REASON_SIGINT ):
+         fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec );
+         fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec ); 
 
+         break;
+      case( REASON_SIGPIPE ):
+         fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nConnection closed!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec );
+         fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nConnection closed!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec ); 
+         break;
+      default:
+         break;
+   }
    fclose( log );
    free( msg_in );
    free( msg_out );
+   close( sock_fd );
    exit(0);
 }
 
@@ -38,59 +60,53 @@ void sig_handler(int signo)
 {
    if( signo == SIGINT )
    {
-      socket_exit();
+      socket_exit( REASON_SIGINT );
    }
-   else
+   else if( signo == SIGPIPE )
    {
-      fprintf( stdout, "PID %d caught unknown signal!\n",
-               getpid() );
-      fprintf( log, "PID %d caught unknown signal!\n",
-               getpid() );
+      socket_exit( REASON_SIGPIPE );
    }
    return;
 }
 
 int main(void)
 {
-   int sock;
-   struct sockaddr_in their_addr;
-
-   char *msg = sequence_a[0];
+   struct sockaddr_in serv_addr;
 
    /* Set up signal handling */
    signal( SIGINT, sig_handler );
-
+   signal( SIGPIPE, sig_handler );
    /* Log file */
-   log = fopen( "second_pid.log", "w" );
+   log = fopen( "client.log", "w" );
 
    fprintf( log, "PID %d - Sockets:\n",
             getpid());
 
-   if( 0 > (sock = socket( AF_INET, SOCK_STREAM, 0 )) )
+   if( 0 > (sock_fd = socket( AF_INET, SOCK_STREAM, 0 )) )
    {
       perror( "Encountered error creating socket" );
       return 1;
    }
 
-   memset( &their_addr, '0', sizeof(their_addr) );
+   memset( &serv_addr, '0', sizeof(serv_addr) );
 
-   their_addr.sin_family = AF_INET;
-   their_addr.sin_port = htons ( PORT );
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_port = htons ( PORT );
 
-   if( 0 >= inet_pton( AF_INET, "127.0.0.1", &their_addr.sin_addr ) )
+   if( 0 >= inet_pton( AF_INET, "127.0.0.1", &serv_addr.sin_addr ) )
    {
       perror( "Error" );
       return 1;
    }
 
-   if( 0 > connect(sock, (struct sockaddr *)&their_addr, sizeof(their_addr) ) )
+   if( 0 > connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr) ) )
    {
       perror( "Encountered error accepting new connection" );
       return 1;
    }
 
-   fprintf( log, "Socket ID: %d\nSocket Option: %d\nFamily: %d\nSource Address: %d\n",
-            sock, 8080, AF_INET, INADDR_ANY );
+   fprintf( log, "Socket ID: %d\nPORT: %d\nFamily: %d\nSource Address: %d\n",
+            sock_fd, 8080, AF_INET, INADDR_ANY );
 
    /* Allocate memory for packet struct */
    msg_in = malloc( sizeof(packet_t) );
@@ -110,50 +126,10 @@ int main(void)
    int i = 0;
    while( 10 != i )
    {
-      send( socket, msg, sizeof(*msg), 0 );
-      
-      read( socket, msg_in, sizeof(*msg_in) );
-      fprintf( log, "\n( %ld. %ld secs ) - Received:\nHeader: %sBody: %s",
-               thread_time.tv_sec, thread_time.tv_nsec, msg_in->header, msg_in->body );
-      if( NULL != sequence_a[i] )
+      /* Read from socket */
+      if( 0 > read( sock_fd, msg_in, sizeof(*msg_in) ) )
       {
-         /* Write string to body of message */
-         sprintf( msg_out->body, "%s", sequence_a[i] );
-         msg_out->type = MESSAGE_PRINT;
-
-         sprintf( msg_out->header, "From PID %d - Length %ld\n",
-               getpid(), strlen(sequence_a[i]) );
-      }
-      else
-      {
-         /* This is a command */
-         msg_out->type = MESSAGE_CMD;
-      }
-
-      /* Write full message to pipe */
-      if( 0 > write( fout, msg_out, sizeof(*msg_out) ) )
-      {
-         perror( "Encountered error while attempting to write to FIFO!\n" );
-      }
-
-      clock_gettime( CLOCK_REALTIME, &thread_time );
-
-      if( MESSAGE_PRINT == msg_out->type )
-      {
-         fprintf( log, "\n( %ld. %ld secs ) - Sending: %s",
-                  thread_time.tv_sec, thread_time.tv_nsec, msg_out->body );
-      }
-      else
-      {
-         /* This is a command, so we don't want to print it - just handle it */
-         fprintf( log, "\n( %ld. %ld secs ) - Sending: COMMAND",
-                  thread_time.tv_sec, thread_time.tv_nsec );
-      }
-
-      /* Read from Input FIFO */
-      if( 0 > read( fin, msg_in, sizeof(*msg_in) ) )
-      {
-         perror("Encountered error while atttempting to read FIFO!\n" );
+         perror("Encountered error while atttempting to read socket" );
       }
 
       clock_gettime( CLOCK_REALTIME, &thread_time );
@@ -169,9 +145,47 @@ int main(void)
          fprintf( log, "\n( %ld. %ld secs ) - Received: COMMAND",
                   thread_time.tv_sec, thread_time.tv_nsec );
       }
-      i++;
 
+      if( NULL != sequence_a[i] )
+      {
+         /* Write string to body of message */
+         sprintf( msg_out->body, "%s", sequence_a[i] );
+         msg_out->type = MESSAGE_PRINT;
+
+         sprintf( msg_out->header, "From PID %d - Length %ld\n",
+               getpid(), strlen(sequence_a[i]) );
+      }
+      else
+      {
+         /* This is a command */
+         msg_out->type = MESSAGE_CMD;
+      }
+
+      /* Write full message to socket */
+      if( 0 > send( sock_fd, msg_out, sizeof(*msg_out), 0) )
+      {
+         perror( "Encountered error while attempting to write to socket" );
+      }
+
+      clock_gettime( CLOCK_REALTIME, &thread_time );
+
+      if( MESSAGE_PRINT == msg_out->type )
+      {
+         fprintf( log, "\n( %ld. %ld secs ) - Sending: %s",
+                  thread_time.tv_sec, thread_time.tv_nsec, msg_out->body );
+      }
+      else
+      {
+         /* This is a command, so we don't want to print it - just handle it */
+         fprintf( log, "\n( %ld. %ld secs ) - Sending: COMMAND",
+                  thread_time.tv_sec, thread_time.tv_nsec );
+ 
+      } 
+      i++;
+      if( 10 == i )
+      {
+         i = 0;
+      }
    }
-   socket_exit();
    return 0;
 }
