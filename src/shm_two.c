47e6d52ed99ba2bc4119df8d37074685d4eb7d75
Roberto Baquerizo <roba8460@colorado.edu>
2019-02-25 23:17:23 -0700

    shared memory ipc implementation

---
+++ src/shm_two.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+#include <semaphore.h>
+#include <errno.h>
+/* /sys includes */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+/* Local Inlcudes */
+#include "common.h"
+#define SHM_SEGMENT_SIZE 65536
+#define SHM_SEGMENT_NAME "/demo-shm"
+#define SEMA_NAME "/demo-sem"
+
+static sem_t *sem;
+
+static FILE     *log;
+static packet_t *msg_in;
+static packet_t *msg_out;
+
+static struct timespec thread_time;
+
+typedef enum reasons {
+   REASON_BEGIN,
+   REASON_SIGINT,
+   REASON_SIGPIPE,
+   REASON_MAX
+} reason_e;
+
+static void *get_shared_memory(void)
+{
+   int shm_fd;
+   struct shared_data *shm_p;
+
+   shm_fd = shm_open( SHM_SEGMENT_NAME, O_CREAT | O_EXCL | O_RDWR, 0666 );
+   if( 0 < shm_fd )
+   {
+      fprintf( stdout, "Creating shared memroy and setting size to %d bytes\n",
+               SHM_SEGMENT_SIZE );
+      fprintf( log, "Creating shared memroy and setting size to %d bytes\n",
+               SHM_SEGMENT_SIZE );
+
+      if( 0 > ftruncate( shm_fd, SHM_SEGMENT_SIZE) )
+      {
+         perror( "Encountered error setting size of shared memroy" );
+         exit(1);
+      }
+
+      sem = sem_open( SEMA_NAME, O_RDWR | O_CREAT, 0666, 1 );
+
+		if( SEM_FAILED == sem )
+      {
+			perror( "Encountered error creating semapahore for shared memory" );
+         exit(1);
+      }
+	}
+   else if( -1 == shm_fd && EEXIST == errno )
+   {
+		/* Already exists: open again without O_CREAT */
+		shm_fd = shm_open(SHM_SEGMENT_NAME, O_RDWR, 0);
+		sem = sem_open(SEMA_NAME, O_RDWR);
+
+		if( SEM_FAILED == sem )
+      {
+			perror( "Encountered error creating semapahore for shared memory" );
+         exit(1);
+      }
+	}
+   else if (shm_fd == -1)
+   {
+		perror( "shm_open " SHM_SEGMENT_NAME );
+		exit(1);
+	}
+
+	/* Map the shared memory */
+	shm_p = mmap(NULL, SHM_SEGMENT_SIZE, PROT_READ | PROT_WRITE,
+		     MAP_SHARED, shm_fd, 0);
+
+	if (shm_p == NULL) {
+		perror( "Encountered error memory mapping shared memory" );
+		exit(1);
+	}
+	return shm_p;
+}
+
+static void shm_exit( reason_e reason )
+{
+   clock_gettime(CLOCK_REALTIME, &thread_time);
+
+   switch( reason )
+   {
+      case( REASON_SIGINT ):
+         fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec );
+         fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nCaught SIGINT signal!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec ); 
+         break;
+      case( REASON_SIGPIPE ):
+         fprintf( stdout, "\nPID [%d] ( %ld.%ld secs )\nConnection closed!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec );
+         fprintf( log, "\nPID [%d] ( %ld.%ld secs )\nConnection closed!\n",
+                  getpid(), thread_time.tv_sec, thread_time.tv_nsec ); 
+         break;
+      default:
+         break;
+   }
+   fclose( log );
+   free( msg_in );
+   free( msg_out );
+   shm_unlink( SHM_SEGMENT_NAME );
+   exit(0);
+}
+
+void sig_handler(int signo)
+{
+   if( signo == SIGINT )
+   {
+      shm_exit( REASON_SIGINT );
+   }
+   else if( signo == SIGPIPE )
+   {
+      shm_exit( REASON_SIGPIPE );
+   }
+   return;
+}
+
+int main(void)
+{
+   /* Set up signal handling */
+   signal( SIGINT, sig_handler );
+   signal( SIGPIPE, sig_handler );
+
+   /* Log file */
+   log = fopen( "shm_two.log", "w" );
+
+   fprintf( log, "PID %d - Shared Memory:\n",
+            getpid() );
+
+   packet_t *shm_p = get_shared_memory();
+
+   /* Allocate memory for packet struct */
+   msg_in = malloc( sizeof(packet_t) );
+   if( NULL == msg_in )
+   {
+      fprintf( stderr, "Encountered error allocating memory for packet struct!\n" );
+      return 1;
+   }
+
+   msg_out = malloc( sizeof(packet_t) );
+   if( NULL == msg_out )
+   {
+      fprintf( stderr, "Encountered error allocating memory for packet struct!\n" );
+      return 1;
+   }
+
+   int i = 0;
+   while( 10 != i )
+   {
+      /* Read from Input FIFO */
+      sem_wait( sem );
+      memcpy( msg_in, shm_p, sizeof(*shm_p) );
+      sem_post( sem );
+
+      clock_gettime( CLOCK_REALTIME, &thread_time );
+
+      if( MESSAGE_PRINT == msg_in->type )
+      {
+         fprintf( log, "\n( %ld. %ld secs ) - Received:\nHeader: %sBody: %s",
+                  thread_time.tv_sec, thread_time.tv_nsec, msg_in->header, msg_in->body );
+      }
+      else
+      {
+         /* This is a command, so we don't want to print it - just handle it */
+         fprintf( log, "\n( %ld. %ld secs ) - Received: COMMAND",
+                  thread_time.tv_sec, thread_time.tv_nsec );
+      }
+
+      if( NULL != sequence_b[i] )
+      {
+         /* Write string to body of message */
+         sprintf( msg_out->body, "%s", sequence_b[i] );
+         msg_out->type = MESSAGE_PRINT;
+
+         sprintf( msg_out->header, "From PID %d - Length %ld\n",
+                  getpid(), strlen(sequence_b[i]) );
+      }
+      else
+      {
+         /* This is a command */
+         msg_out->type = MESSAGE_CMD;
+      }
+
+      /* Write full message to pipe */
+      sem_wait( sem );
+      memcpy( shm_p, msg_out, sizeof(*msg_out));
+      sem_post( sem );
+
+      clock_gettime( CLOCK_REALTIME, &thread_time );
+
+      if( MESSAGE_PRINT == msg_out->type )
+      {
+         fprintf( log, "\n( %ld. %ld secs ) - Sending: %s",
+                  thread_time.tv_sec, thread_time.tv_nsec, msg_out->body );
+      }
+      else
+      {
+         /* This is a command, so we don't want to print it - just handle it */
+         fprintf( log, "\n( %ld. %ld secs ) - Sending: COMMAND",
+                  thread_time.tv_sec, thread_time.tv_nsec );
+      }
+
+      i++;
+      if( 10 == i )
+      {
+         i = 0;
+      }
+   }
+   return 0;
+}
